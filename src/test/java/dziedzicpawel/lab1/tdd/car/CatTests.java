d4dff0f86fab20f80044f8b8ad6caeb2b14e053e
Dziedzic Paweł <w63622@student.wsiz.rzeszow.pl>
2021-03-21 10:42:49 +0100

    4.Test shouldHaveCorrectRefuel

--- src/test/java/dziedzicpawel/lab1/tdd/car/CatTests.java
+++ src/test/java/dziedzicpawel/lab1/tdd/car/CatTests.java
@@ -16,4 +16,17 @@ public class CatTests {
         Assertions.assertEquals(car.getFuelConsumption(), 5.4);
         Assertions.assertEquals(car.getTankCapacity(), 70);
     }
+
+    @Test
+    public void shouldHaveCorrectRefuel() {
+        Car car = new Car("red", "Skoda Octavia", 5.4, 70);
+
+        boolean extraLitresOfFuel1 = car.refuel(100);
+        boolean extraLitresOfFuel2 = car.refuel(-50);
+        boolean extraLitresOfFuel3 = car.refuel(10);
+
+        Assertions.assertFalse(extraLitresOfFuel1);
+        Assertions.assertFalse(extraLitresOfFuel2);
+        Assertions.assertTrue(extraLitresOfFuel3);
+    }
 }
