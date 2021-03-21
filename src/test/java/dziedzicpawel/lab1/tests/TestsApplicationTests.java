e7b0794ed75fd815d71176d1698d66ec7da1b7ed
Dziedzic Paweł <w63622@student.wsiz.rzeszow.pl>
2021-03-21 08:49:56 +0100

    init commit

---
+++ src/test/java/dziedzicpawel/lab1/tests/TestsApplicationTests.java
@@ -0,0 +1,13 @@
+package dziedzicpawel.lab1.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
