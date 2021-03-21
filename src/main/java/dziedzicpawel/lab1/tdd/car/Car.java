4c0c970a0d0a329ed73a2f46c39bf351337bab15
Dziedzic Paweł <w63622@student.wsiz.rzeszow.pl>
2021-03-21 10:52:06 +0100

    5.Add method refuel in class Car

--- src/main/java/dziedzicpawel/lab1/tdd/car/Car.java
+++ src/main/java/dziedzicpawel/lab1/tdd/car/Car.java
@@ -6,15 +6,21 @@ public class Car {
     private String make;
     private double fuelConsumption;
     private int tankCapacity;
-    private double fuelLevel;
-    private double odometer;
-    private double dailyOdometer;
+    private double fuelLevel=20;
+    private double odometer=100000.9;
+    private double dailyOdometer= 801.2;
 
     public Car(String color, String make, double fuelConsumption, int tankCapacity) {
         this.color=color;
         this.make=make;
         this.fuelConsumption=fuelConsumption;
         this.tankCapacity=tankCapacity;
+    }
+
+    public boolean refuel(int litres) {
+        if(litres <0)
+            return false;
+        else return !(litres + getFuelLevel() > getTankCapacity());
     }
 
     public String getColor() {
@@ -31,5 +37,17 @@ public class Car {
 
     public int getTankCapacity() {
         return tankCapacity;
+    }
+
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public double getOdometer() {
+        return odometer;
+    }
+
+    public double getDailyOdometer() {
+        return dailyOdometer;
     }
 }
